847b60a9fc6518dd0626c668a74a064c87e2153b
haixuanTao <tao.xavier@outlook.com>
2023-10-19 15:15:41 +0200

    Bump opentelemetry version to `0.18`

--- src/lib.rs
+++ src/lib.rs
@@ -17,15 +17,14 @@
 //! ```
 //!
 use opentelemetry::metrics::Unit;
-use std::sync::Arc;
-use std::sync::Mutex;
+
 use sysinfo::PidExt;
 
 use sysinfo::ProcessExt;
 use sysinfo::SystemExt;
 use sysinfo::{get_current_pid, System};
 
-use opentelemetry::metrics::{BatchObserverResult, Meter};
+use opentelemetry::metrics::Meter;
 use opentelemetry::Key;
 
 const PROCESS_PID: Key = Key::from_static_str("process.pid");
@@ -59,44 +58,36 @@ const DIRECTION: Key = Key::from_static_str("direction");
 // ```
 //
 pub fn init_process_observer(meter: Meter) {
-    let sys = Arc::new(Mutex::new(System::new_all()));
-    let mut sys_lock = sys.lock().unwrap();
-    sys_lock.refresh_all();
-
     let pid = get_current_pid().unwrap();
-    let core_count = sys_lock.physical_core_count().unwrap();
+    let sys_ = System::new_all();
+    let core_count = sys_.physical_core_count().unwrap();
+    let process_cpu_utilization = meter
+        .f64_observable_gauge(PROCESS_CPU_USAGE)
+        .with_description("The percentage of CPU in use.")
+        .init();
+    let process_cpu_usage = meter.f64_observable_gauge(PROCESS_CPU_UTILIZATION).init();
+    let process_memory_usage = meter
+        .i64_observable_gauge(PROCESS_MEMORY_USAGE)
+        .with_description("The amount of physical memory in use.")
+        .with_unit(Unit::new("byte"))
+        .init();
+    let process_memory_virtual = meter
+        .i64_observable_gauge(PROCESS_MEMORY_VIRTUAL)
+        .with_description("The amount of committed virtual memory.")
+        .with_unit(Unit::new("byte"))
+        .init();
+    let process_disk_io = meter
+        .i64_observable_gauge(PROCESS_DISK_IO)
+        .with_description("Disk bytes transferred.")
+        .with_unit(Unit::new("byte"))
+        .init();
 
     meter
-        .build_batch_observer(|batch| {
-            let process_cpu_utilization = batch
-                .f64_value_observer(PROCESS_CPU_USAGE)
-                .with_description("The percentage of CPU in use.")
-                .init();
-            let process_cpu_usage = batch.f64_value_observer(PROCESS_CPU_UTILIZATION).init();
-            let process_memory_usage = batch
-                .i64_value_observer(PROCESS_MEMORY_USAGE)
-                .with_description("The amount of physical memory in use.")
-                .with_unit(Unit::new("byte"))
-                .init();
-            let process_memory_virtual = batch
-                .i64_value_observer(PROCESS_MEMORY_VIRTUAL)
-                .with_description("The amount of committed virtual memory.")
-                .with_unit(Unit::new("byte"))
-                .init();
-            let process_disk_io = batch
-                .i64_value_observer(PROCESS_DISK_IO)
-                .with_description("Disk bytes transferred.")
-                .with_unit(Unit::new("byte"))
-                .init();
-            // let process_network_io = batch
-            //     .i64_value_observer(PROCESS_NETWORK_IO)
-            //     .with_description("All network bytes transferred.")
-            //     .with_unit(Unit::new("byte"))
-            //     .init();
+        .register_callback(move |context| {
+            let mut sys = System::new_all();
+            sys.refresh_processes();
 
-            let sys = sys.clone();
-
-            let common_attributes = if let Some(process) = sys_lock.process(pid) {
+            let common_attributes = if let Some(process) = sys.process(pid) {
                 [
                     PROCESS_PID.i64(pid.as_u32().into()),
                     PROCESS_EXECUTABLE_NAME.string(process.name().to_string()),
@@ -107,55 +98,54 @@ pub fn init_process_observer(meter: Meter) {
                 unimplemented!()
             };
 
-            Ok(move |result: BatchObserverResult| {
-                let mut sys_lock = sys.lock().unwrap();
+            sys.refresh_process(pid);
 
-                sys_lock.refresh_process(pid);
+            if let Some(process) = sys.process(pid) {
+                let cpu_usage = process.cpu_usage() / 100.;
+                let disk_io = process.disk_usage();
+                // let network_io = process.network_usage();
+                process_cpu_usage.observe(context, cpu_usage.into(), &[]);
+                process_cpu_utilization.observe(
+                    context,
+                    (cpu_usage / core_count as f32).into(),
+                    &common_attributes,
+                );
+                process_memory_usage.observe(
+                    context,
+                    (process.memory() * 1_000).try_into().unwrap(),
+                    &common_attributes,
+                );
+                process_memory_virtual.observe(
+                    context,
+                    (process.virtual_memory() * 1_000).try_into().unwrap(),
+                    &common_attributes,
+                );
+                process_disk_io.observe(
+                    context,
+                    disk_io.read_bytes.try_into().unwrap(),
+                    &[common_attributes.as_slice(), &[DIRECTION.string("read")]].concat(),
+                );
+                process_disk_io.observe(
+                    context,
+                    disk_io.written_bytes.try_into().unwrap(),
+                    &[common_attributes.as_slice(), &[DIRECTION.string("write")]].concat(),
+                );
 
-                if let Some(process) = sys_lock.process(pid) {
-                    let cpu_usage = process.cpu_usage() / 100.;
-                    let disk_io = process.disk_usage();
-                    // let network_io = process.network_usage();
-                    result.observe(&[], &[process_cpu_usage.observation(cpu_usage.into())]);
-                    result.observe(
-                        &common_attributes,
-                        &[process_cpu_utilization
-                            .observation((cpu_usage / core_count as f32).into())],
-                    );
-                    result.observe(
-                        &common_attributes,
-                        &[process_memory_usage
-                            .observation((process.memory() * 1_000).try_into().unwrap())],
-                    );
-                    result.observe(
-                        &common_attributes,
-                        &[process_memory_virtual
-                            .observation((process.virtual_memory() * 1_000).try_into().unwrap())],
-                    );
-                    result.observe(
-                        &[common_attributes.as_slice(), &[DIRECTION.string("read")]].concat(),
-                        &[process_disk_io.observation(disk_io.read_bytes.try_into().unwrap())],
-                    );
-                    result.observe(
-                        &[common_attributes.as_slice(), &[DIRECTION.string("write")]].concat(),
-                        &[process_disk_io.observation(disk_io.written_bytes.try_into().unwrap())],
-                    );
-                    // result.observe(
-                    //     &[common_attributes.as_slice(), &[DIRECTION.string("receive")]].concat(),
-                    //     &[process_network_io
-                    //         .observation(network_io.received_bytes.try_into().unwrap())],
-                    // );
-                    // result.observe(
-                    //     &[
-                    //         common_attributes.as_slice(),
-                    //         &[DIRECTION.string("transmit")],
-                    //     ]
-                    //     .concat(),
-                    //     &[process_network_io
-                    //         .observation(network_io.transmitted_bytes.try_into().unwrap())],
-                    // );
-                }
-            })
+                // result.observe(
+                //     &[common_attributes.as_slice(), &[DIRECTION.string("receive")]].concat(),
+                //     &[process_network_io
+                //         .observe(context,(network_io.received_bytes.try_into().unwrap())],
+                // );
+                // result.observe(
+                //     &[
+                //         common_attributes.as_slice(),
+                //         &[DIRECTION.string("transmit")],
+                //     ]
+                //     .concat(),
+                //     &[process_network_io
+                //         .observe(context,(network_io.transmitted_bytes.try_into().unwrap())],
+                // );
+            }
         })
         .unwrap();
 }
